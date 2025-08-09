cb51616e2e18a6afb8edbd6a2e3c5b6ea03c8965
Nik_SSD <nik_rink@mail.ru>
2025-08-09 20:29:43 +0300

    Убрал лишнее 2

---
+++ config-service/src/test/java/ru/nikrink/configservice/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+//package ru.nikrink.configservice;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class ConfigServiceApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
